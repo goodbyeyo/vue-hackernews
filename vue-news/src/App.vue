b0cb5f9b2785f4cc422ce9809819a877cd4e5e8b
yoonseol <yoonseol1983@gmail.com>
2021-04-03 15:26:46 +0900

    라우터 설정 및 툴바 기본 UI 추가

---
+++ vue-news/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- URL이 만약 news라면  <news-view></news-view> -->
+    
+    <!-- 1. Strongly Recommented-->
+    <!-- <ToolBar></ToolBar> <== 앞글자 대문자 : 컴포넌트 태그인것이 명확히 구별됨-->
+    
+    <!-- 2. Essentail Recommented-->
+    <tool-bar></tool-bar>   <!-- 컴포넌트 태그 연결 가능 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue';
+
+export default {
+  components: {
+    ToolBar,
+  },
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link to="/news">News</router-link>
+        &nbsp;&nbsp;
+        <router-link to="/ask">Ask</router-link>
+        &nbsp;&nbsp;
+        <router-link to="/jobs">Jobs</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
