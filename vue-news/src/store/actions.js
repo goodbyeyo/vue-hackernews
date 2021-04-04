dd771eeb5367e42b767856294719fed8cf9b3781
yoonseol <yoonseol1983@gmail.com>
2021-04-04 15:05:41 +0900

    store 속성 모듈화

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,33 @@
+export default {
+    FETCH_NEWS(context) {
+        fetchNewsList()
+            .then(response => {
+                console.log(response.data);
+                context.commit('SET_NEWS', response.data);
+            })
+            .catch(error => {
+                console.log(error);
+        })
+    },
+    FETCH_JOBS({ commit }) {
+        fetchJobsList()
+            .then(({ data }) => {   
+                console.log(data);
+                console.log(context);
+                commit('SET_JOBS', data);   // mutations
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },
+    FETCH_ASK({ commit }) {
+        fetchAskList()
+            .then(({data}) => {   
+                console.log(data);
+                commit('SET_ASKS', data);   // mutations 호출
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+}
