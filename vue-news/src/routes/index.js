658a3475a16aec418e903e66049454442d19f697
yoonseol <yoonseol1983@gmail.com>
2021-04-04 20:06:02 +0900

    detail page 구현(router-link, param 전달)

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -30,14 +30,12 @@ export const router = new VueRouter({
             component: JobsView,
         },
         {
-            path: '/user',
+            path: '/user/:id',
             component: UserView,
-
         },
         {
             path: '/item',
             component: ItemView
-
         },
     ]
 })
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -5,10 +5,15 @@
     <!-- <div v-for="user in users">{{ user.title }}</div> -->
     <!-- <div v-for="user in this.$store.state.news">{{ user.title }}</div> -->
     <p v-for="item in this.$store.state.news">
-      <!-- <a href="http://google.com"> {{ item.title }} </a> -->
-      <a v-bind:href="item.url"> {{ item.title }} </a>
-      <small>{{item.time_ago}} by {{ item.user }} </small>
-    </p> -->
+      <a v-bind:href="item.url"> 
+        {{ item.title }} 
+        </a>
+      <small>
+        {{item.time_ago}} by 
+        <!-- <router-link v-bind:to="'/user/' + item.user">{{ item.user }} </router-link> -->
+        <router-link v-bind:to="`/user/${item.user}`">{{ item.user }} </router-link>
+      </small>
+    </p>
   </div>
 </template>
 
