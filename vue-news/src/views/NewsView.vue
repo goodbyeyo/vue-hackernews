91b8ec8446cf229c014d7df0b4a3bd862985f57e
yoonseol <yoonseol1983@gmail.com>
2021-04-20 16:39:33 +0900

    change newsView to common ListView

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,37 +1,25 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li v-for="item in this.$store.state.news" class="post">
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points}}
-        </div> 
-        <!-- 기타 정보 영역 -->
-        <p class='news-title'>
-          <a v-bind:href="item.url">
-            {{ item.title }} 
-          </a>
-        </p>
-        <small class="link-text">
-           {item.time_ago}} by 
-          <router-link v-bind:to="`/user/${item.user}`">{{ item.user }} </router-link>
-        </small>
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
+import ListItem from '../components/ListItem.vue';
 // import {fetchNewsList} from '../api/index.js';
 
 export default {
+  components: {
+    ListItem,
+  }
+}
   // data() {
   //   return {
   //     users: []
   //   }
   // },
-  created() {
-    this.$store.dispatch('FETCH_NEWS'); 
+  // created() {
+  //   this.$store.dispatch('FETCH_NEWS'); 
     // console.log('호출전: ', this); //   호출전에는 VueComponent를 가르킨다
     // 다른언어는 this는 지역 scope 유효범위 좁게, 자바스크립트는 this 는 전역으로 시작
     // ES6 에서 use strict 문법 존재
@@ -59,38 +47,10 @@ export default {
     //   .catch(function(error){
     //      console.log(error)
     //   })
-  },
-}
+  // },
+// }
 </script>
 
 <style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;  /* 수직 정렬 */
-  justify-content: center; /* 가로 정렬 */
-  color: #42b883;
-}
-
-.news-title {
-  margin: 0;
-}
-
-.link-text {
-  color: #828282
-}
 
 </style>
