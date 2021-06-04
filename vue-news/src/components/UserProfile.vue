8d963fd01dd1fa759f0f67fb5fe58c70830d2445
yoonseol <yoonseol1983@gmail.com>
2021-06-04 10:42:54 +0900

    UserProfile props add, computed delete

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="user-container">
+            <div>
+                <i class="fas fa-user"></i>
+            </div>
+            <div class="user-description">
+               <div>
+                   {{ info.id }}
+                </div>
+                <div class="time">
+                    {{ info.created }}
+                </div>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        info: Object
+    },
+    // props로 내려온 info를 전달하기때문에 computed 속성은 필요없음
+    // computed: {
+    //     userInfo(){
+    //         return this.$store.state.user;
+    //     }
+    // },
+
+}
+</script>
+
+<style scoped>
+.user-container {
+    /* div가 수평으로  */
+    display: flex;  
+    align-items: center;
+    padding: 0.5rem;
+}
+.fa-user {
+    font-size: 2.0rem;
+
+}
+.user-description {
+    padding: 8px;
+}
+.time {
+    font-size: 7px
+}
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
+      <user-profile :info="userInfo"></user-profile>  <!-- computed 속성으로 내려받음-->
+      <!-- 
       <p> name : {{ this.$store.state.user.id}} </p>
       <p> karma : {{ this.$store.state.user.karma}} </p>
-      <p> created : {{ this.$store.state.user.created}} </p>
-      
+      <p> created : {{ this.$store.state.user.created}} </p> 
+      -->
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import UserProfile from '../components/UserProfile.vue';
+
 
 export default {
+  components: {
+    UserProfile,
+  },
+  computed: {
+    userInfo(){
+      return this.$store.state.user;
+    }
+  },
   created() {
-    console.log(this.$route.params.id);
     const userName = this.$route.params.id;
     // userName을 dispacth 를 통해 store로 전달해서 API 호출
     this.$store.dispatch('FETCH_USER', userName);
