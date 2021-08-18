06b0cca1605aa85590ba1d13ecf0eb68055e9ffb
yoonseol <yoonseol1983@gmail.com>
2021-08-18 15:10:54 +0900

    return add for async process

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -11,7 +11,7 @@ import {
 export default {
     // high order component 구현으로 불필요한 action function 제거
     FETCH_NEWS(context) {
-        fetchNewsList()
+        return fetchNewsList()  // 메서드 체이닝을 위해 return이 있어야 비동기처리가능
             .then(response => {
                 // 데이터를 꺼내와서 담은 다음 mutation에 담아주고 응답데이터를 화면으로 리턴
                 context.commit('SET_NEWS', response.data);  
@@ -24,7 +24,7 @@ export default {
         })
     },
     FETCH_JOBS({ commit }) {
-        fetchJobsList()
+        return fetchJobsList()
             .then(({ data }) => {   
                 console.log(data);
                 commit('SET_JOBS', data);   // mutations
@@ -34,7 +34,7 @@ export default {
             })
     },
     FETCH_ASK({ commit }) {
-        fetchAskList()
+        return fetchAskList()
             .then(({ data }) => {   
                 console.log(data);
                 commit('SET_ASKS', data);   // mutations 호출
@@ -44,7 +44,7 @@ export default {
             })
     },
     FETCH_USER({ commit }, name) {
-        fetchUserInfo(name)
+        return fetchUserInfo(name)
             .then(({ data }) => {
                 console.log(data);
                 commit('SET_USER', data);   // mutations 호출
@@ -54,7 +54,7 @@ export default {
             })
     },
     FETCH_ITEM({ commit }, id) {
-        fetchCommentItem(id)
+        return fetchCommentItem(id)
             .then(({ data }) => {
                 console.log(data);
                 commit('SET_ITEM', data);   // mutations 호출
@@ -64,7 +64,7 @@ export default {
             })
     },
     FETCH_LIST({ commit }, pageName) {
-        fetchList(pageName)
+        return fetchList(pageName)
             .then(({ data }) => {
                 console.log(data);
                 commit('SET_LIST', data);   // mutations 호출
